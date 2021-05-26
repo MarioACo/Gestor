a2bbe4f4592f47b75b300760b877ec9d53b7f857
MarioACo <81332478+MarioACo@users.noreply.github.com>
2021-05-26 06:46:35 +0100

    heroku

--- clases/conexion.php
+++ clases/conexion.php
@@ -2,10 +2,14 @@
 
     class Conectar{
         public function conexion(){
-            $servidor = "localhost";
+            /*$servidor = "localhost";
             $usuario = "root";
             $password = "";
-            $base = "gestor";
+            $base = "gestor";*/
+            $servidor = "mysql5046.site4now.net";
+            $usuario = "a75039_gestor";
+            $password = "Mafer180899";
+            $base = "db_a75039_gestor";
 
             $conexion = mysqli_connect($servidor,
                                         $usuario,
